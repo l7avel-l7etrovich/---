306645e95231d72960530f936ccf63080578e0ad
Pavel <Pavlik59@icloud.com>
2022-07-05 20:45:26 +0500

    009

---
+++ 009/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 15, 224, 375, 432, 597, 688, 743, 82, 96 };
+
+int num = array.Length;
+int find = 6;
+
+int index = 0;
+while (index < num)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index += 1;
+}
